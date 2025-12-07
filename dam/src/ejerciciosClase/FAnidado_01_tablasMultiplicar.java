4ae8c2c070b5953c7e0ee2f65d528e1e5e249932
AdriReyDev <243771752+AdriReyDev@users.noreply.github.com>
2025-12-07 13:07:23 +0100

    Completa FAnidado_01_tablasMultiplicar

---
+++ dam/src/ejerciciosClase/FAnidado_01_tablasMultiplicar.java
@@ -0,0 +1,31 @@
+/*
+ Crear un programa que muestre las tablas de multiplicar del 1 al 10. 
+ */
+package ejerciciosClase;
+
+
+public class FAnidado_01_tablasMultiplicar {
+    
+    public static void main(String[] args) {
+        
+          int count = 1;
+          
+    
+        for (int j = 1; j <= 10; j++) {
+     
+            
+            for (int i = 1; i <= 10 ; i++) {
+         
+                count = j * i;
+                System.out.println(count);
+                
+            }
+            
+        }
+        
+        
+    }
+    
+        
+}
+    
